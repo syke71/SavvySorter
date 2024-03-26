98bb01c9836e3cda360c8d0df2ba411f08d97307
uenqh <uenqh@student.kit.edu>
2024-03-26 18:47:50 +0100

    fixed testcase(s)

--- src/edu/kit/uenqh/model/files/FileHandler.java
+++ src/edu/kit/uenqh/model/files/FileHandler.java
@@ -1,17 +1,13 @@
 package edu.kit.uenqh.model.files;
 
-import edu.kit.uenqh.model.SortingSystem;
 import edu.kit.uenqh.model.files.tags.MultiValueTag;
 import edu.kit.uenqh.model.files.tags.NumericTag;
 import edu.kit.uenqh.model.files.tags.Tag;
 import edu.kit.uenqh.model.files.tags.TagFactory;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-
 import static edu.kit.uenqh.model.files.FileConstants.MULTI_VALUE_TAG_NAME;
 
 /**
@@ -19,39 +15,30 @@ import static edu.kit.uenqh.model.files.FileConstants.MULTI_VALUE_TAG_NAME;
  *
  * @author uenqh
  */
-public class FileHandler {
+public final class FileHandler {
     // Tag conversions
-    private static final String NUMERIC_SIZE_TAG_NAME = "Size";
-    private static final String IMAGE_SIZE_TAG_NAME = "ImageSize";
-    private static final String NUMERIC_LENGTH_TAG_NAME = "Length";
-    private static final String AUDIO_LENGTH_TAG_NAME = "AudioLength";
-    private static final String VIDEO_LENGTH_TAG_NAME = "VideoLength";
-    private static final String WORDS_TAG_NAME = "Words";
-    private static final String TEXT_LENGTH_TAG_NAME = "TextLength";
-    private static final String GENRE_TAG_NAME = "Genre";
-    private static final String AUDIO_GENRE_TAG_NAME = "AudioGenre";
-    private static final String VIDEO_GENRE_TAG_NAME = "VideoGenre";
-    private static final String TEXT_GENRE_TAG_NAME = "TextGenre";
+    private static final String NUMERIC_SIZE_TAG_NAME = "size";
+    private static final String IMAGE_SIZE_TAG_NAME = "imagesize";
+    private static final String NUMERIC_LENGTH_TAG_NAME = "length";
+    private static final String AUDIO_LENGTH_TAG_NAME = "audiolength";
+    private static final String VIDEO_LENGTH_TAG_NAME = "videolength";
+    private static final String WORDS_TAG_NAME = "words";
+    private static final String TEXT_LENGTH_TAG_NAME = "textlength";
+    private static final String GENRE_TAG_NAME = "genre";
+    private static final String AUDIO_GENRE_TAG_NAME = "audiogenre";
+    private static final String VIDEO_GENRE_TAG_NAME = "videogenre";
+    private static final String TEXT_GENRE_TAG_NAME = "textgenre";
     private static final String COLLIDING_TAGS_FORMAT = "the entered tags '%s' and '%s' are colliding with each other!";
 
-
-    private final SortingSystem sortingSystem;
-    private final Map<String, Tag> tagMap;
-    private final List<String> fileTypes;
-
     /**
-     * Constructs a new FileHandler with the given sorting system.
-     *
-     * @param sortingSystem the sorting system to be used
+     * Private constructor to prevent instantiation.
      */
-    public FileHandler(SortingSystem sortingSystem) {
-        this.sortingSystem = sortingSystem;
-        this.fileTypes = new ArrayList<>();
-        this.tagMap = new HashMap<>();
+    private FileHandler() {
+
     }
 
     /**
-     * Formats a file record.
+     * Formats a files.
      *
      * @param files the files to be formatted
      * @return the formatted file record
@@ -151,25 +138,25 @@ public class FileHandler {
             for (Tag tag : file.getTags()) {
                 tagNames.add(tag.getName().toLowerCase());
             }
-            if (tagNames.contains(GENRE_TAG_NAME.toLowerCase()) && tagNames.contains(AUDIO_GENRE_TAG_NAME.toLowerCase())) {
+            if (tagNames.contains(GENRE_TAG_NAME.toLowerCase()) && tagNames.contains(AUDIO_GENRE_TAG_NAME)) {
                 message = String.format(COLLIDING_TAGS_FORMAT, GENRE_TAG_NAME, AUDIO_GENRE_TAG_NAME);
             }
-            if (tagNames.contains(GENRE_TAG_NAME.toLowerCase()) && tagNames.contains(TEXT_GENRE_TAG_NAME.toLowerCase())) {
+            if (tagNames.contains(GENRE_TAG_NAME.toLowerCase()) && tagNames.contains(TEXT_GENRE_TAG_NAME)) {
                 message = String.format(COLLIDING_TAGS_FORMAT, GENRE_TAG_NAME, TEXT_GENRE_TAG_NAME);
             }
-            if (tagNames.contains(GENRE_TAG_NAME.toLowerCase()) && tagNames.contains(VIDEO_GENRE_TAG_NAME.toLowerCase())) {
+            if (tagNames.contains(GENRE_TAG_NAME.toLowerCase()) && tagNames.contains(VIDEO_GENRE_TAG_NAME)) {
                 message = String.format(COLLIDING_TAGS_FORMAT, GENRE_TAG_NAME, TEXT_GENRE_TAG_NAME);
             }
-            if (tagNames.contains(NUMERIC_SIZE_TAG_NAME.toLowerCase()) && tagNames.contains(IMAGE_SIZE_TAG_NAME.toLowerCase())) {
+            if (tagNames.contains(NUMERIC_SIZE_TAG_NAME.toLowerCase()) && tagNames.contains(IMAGE_SIZE_TAG_NAME)) {
                 message = String.format(COLLIDING_TAGS_FORMAT, NUMERIC_SIZE_TAG_NAME, IMAGE_SIZE_TAG_NAME);
             }
-            if (tagNames.contains(NUMERIC_LENGTH_TAG_NAME.toLowerCase()) && tagNames.contains(AUDIO_LENGTH_TAG_NAME.toLowerCase())) {
+            if (tagNames.contains(NUMERIC_LENGTH_TAG_NAME.toLowerCase()) && tagNames.contains(AUDIO_LENGTH_TAG_NAME)) {
                 message = String.format(COLLIDING_TAGS_FORMAT, NUMERIC_LENGTH_TAG_NAME, AUDIO_LENGTH_TAG_NAME);
             }
-            if (tagNames.contains(NUMERIC_LENGTH_TAG_NAME.toLowerCase()) && tagNames.contains(VIDEO_LENGTH_TAG_NAME.toLowerCase())) {
+            if (tagNames.contains(NUMERIC_LENGTH_TAG_NAME.toLowerCase()) && tagNames.contains(VIDEO_LENGTH_TAG_NAME)) {
                 message = String.format(COLLIDING_TAGS_FORMAT, NUMERIC_LENGTH_TAG_NAME, VIDEO_LENGTH_TAG_NAME);
             }
-            if (tagNames.contains(WORDS_TAG_NAME.toLowerCase()) && tagNames.contains(TEXT_LENGTH_TAG_NAME.toLowerCase())) {
+            if (tagNames.contains(WORDS_TAG_NAME.toLowerCase()) && tagNames.contains(TEXT_LENGTH_TAG_NAME)) {
                 message = String.format(COLLIDING_TAGS_FORMAT, WORDS_TAG_NAME, TEXT_LENGTH_TAG_NAME);
             }
             if (!message.isEmpty()) {
