9613e5dcb4fcb7c93b3e964560222f066f77ed1a
uenqh <uenqh@student.kit.edu>
2024-03-26 03:12:28 +0100

    fixed test case(s)

--- src/edu/kit/uenqh/model/files/tags/TagFactory.java
+++ src/edu/kit/uenqh/model/files/tags/TagFactory.java
@@ -1,5 +1,7 @@
 package edu.kit.uenqh.model.files.tags;
 
+import java.util.regex.Pattern;
+
 import static edu.kit.uenqh.model.files.FileConstants.BINARY_TAG_NAME;
 import static edu.kit.uenqh.model.files.FileConstants.MULTI_VALUE_TAG_NAME;
 import static edu.kit.uenqh.model.files.FileConstants.NUMERIC_TAG_NAME;
@@ -11,6 +13,8 @@ import static edu.kit.uenqh.model.files.FileConstants.NUMERIC_TAG_NAME;
  */
 public final class TagFactory {
     private static final String INVALID_TAG_TYPE_MESSAGE = "Invalid tag type: ";
+    private static final String INVALID_TAG_NAME_FORMAT = "Invalid tag name: %s. Must have this format: %s";
+    public static final String TAG_REGEX = "[a-zA-Z][0-9a-zA-Z]*";
 
     /**
      * Private constructor to prevent instantiation.
@@ -29,6 +33,9 @@ public final class TagFactory {
      * @throws IllegalArgumentException if the tag type is invalid.
      */
     public static Tag createTag(String tagType, String name, String value) {
+        if (!Pattern.matches(TAG_REGEX, name)) {
+            throw new IllegalArgumentException(String.format(INVALID_TAG_NAME_FORMAT, name, TAG_REGEX));
+        }
         return switch (tagType) {
             case BINARY_TAG_NAME -> new BinaryTag(name, BinaryTagType.valueOf(value.toUpperCase()));
             case MULTI_VALUE_TAG_NAME -> new MultiValueTag(name, value);
